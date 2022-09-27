de00014c4f3c19f1b3c94211bb32c974ed1df972
Sergei US <ustimenko.sv@gmail.com>
2022-09-27 21:54:12 +0300

    Initial commit

---
+++ src/main/java/ru/netology/mytasktwo/DataGenerator.java
@@ -0,0 +1,70 @@
+package ru.netology.mytasktwo;
+
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import jdk.jfr.ContentType;
+import lombok.Value;
+import lombok.val;
+
+import java.util.Locale;
+
+import static ru.netology.mytasktwo.DataGenerator.getRandomLogin;
+import static ru.netology.mytasktwo.DataGenerator.getRandomPassword;
+
+public class DataGenerator {
+
+    private static final RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+    private static final Faker faker = new Faker(new Locale("en"));
+
+    private DataGenerator() {
+    }
+
+    private static void sendRequest(RegistrationDto user) {
+        given()
+                .spec(requestSpec)
+                .body(new RegistrationDto("vasya", "password", "active")) // передаём в теле объект, который будет преобразован в JSON
+                .when() // "когда"
+                .post("/api/system/users") // на какой путь относительно BaseUri отправляем запрос
+                .then() // "тогда ожидаем"
+                .statusCode(200); // код 200 OK
+    }
+
+    public static String getRandomLogin() {
+        return faker.name().firstName().toLowerCase();
+    }
+
+    public static String getRandomPassword() {
+        return faker.internet().password(6, 10, true, false, true);
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static RegistrationDto getUser(String status) {
+            return RegistrationDto(getRandomLogin(), getRandomPassword(), status;
+        }
+
+        public static RegistrationDto getRegisteredUser(String status) {
+            RegistrationDto registeredUser = getUser(status);
+            sendRequest(registeredUser);
+            return registeredUser;
+        }
+    }
+
+    @Value
+    public static class RegistrationDto {
+        String login;
+        String password;
+        String status;
+    }
+}
